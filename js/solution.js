3287002514e9489dd063e828c05453bad2883d1d
Vita <vitac215@gmail.com>
2017-07-29 17:15:09 -0400

    update index hero

--- js/solution.js
+++ js/solution.js
@@ -1,7 +1,6 @@
 $(function() {
     /* detect user agent */
     userAgent = window.navigator.platform;
-    console.log(userAgent);
     if (userAgent !== "MacIntel") {
         $(".prototype iframe").addClass("hide");
     }
